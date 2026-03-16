5597a60bb9a41237a400806dccd01d751d728fee
TNXWasTaken <118904290+TNXWasTaken@users.noreply.github.com>
2026-03-16 12:25:44 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 . "$HOME/.cargo/env"
 pnpm i
-cargo install wasm-bindgen-cli
+cargo install wasm-bindgen-cli @0.2.100
 wget https://github.com/WebAssembly/binaryen/releases/download/version_128/binaryen-version_128-x86_64-linux.tar.gz
 tar -xvzf binaryen-version_128-x86_64-linux.tar.gz
 export PATH="$PWD/binaryen-version_128/bin:$PATH"

09b1780116847715e6e1ac3599d322d3a2da730c
TNXWasTaken <118904290+TNXWasTaken@users.noreply.github.com>
2026-03-16 12:00:58 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ cargo install wasm-bindgen-cli
 wget https://github.com/WebAssembly/binaryen/releases/download/version_128/binaryen-version_128-x86_64-linux.tar.gz
 tar -xvzf binaryen-version_128-x86_64-linux.tar.gz
 export PATH="$PATH:$HOME/binaryen-version_128/bin"
+./binaryen-version_128/bin/wasm-opt --version
 wasm-opt --version
 cargo install wasm-snip
 pnpm rewriter:build

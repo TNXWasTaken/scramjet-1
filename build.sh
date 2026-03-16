6a0bb55a8bdb90ad5db861d9aa08bdb0b879c01d
TNXWasTaken <118904290+TNXWasTaken@users.noreply.github.com>
2026-03-16 11:17:52 +0000

    Update build script to download binaryen
    Replaced cargo add binaryen with curl and tar commands to download and extract binaryen.

--- build.sh
+++ build.sh
@@ -2,7 +2,9 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 . "$HOME/.cargo/env"
 pnpm i
 cargo install wasm-bindgen-cli
-cargo add binaryen
+curl -o binaryen-version_128-x86_64-linux.tar.gz https://github.com/WebAssembly/binaryen/releases/download/version_128/binaryen-version_128-x86_64-linux.tar.gz
+tar -xvf binaryen-version_128-x86_64-linux.tar.gz
+export PATH="$PATH:/binaryen-version_128-x86_64-linux/bin"
 cargo install wasm-snip
 pnpm rewriter:build
 pnpm build

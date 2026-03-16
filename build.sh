8ea5f0b39cb52d613d3bf6d3ec05c015c4ece4df
TNXWasTaken <118904290+TNXWasTaken@users.noreply.github.com>
2026-03-16 10:44:39 +0000

    Update build.sh to install Rust and dependencies
    Added commands to install dependencies and build project.

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 . "$HOME/.cargo/env"
-rustc --version
+pnpm i
+cargo install wasm-bindgen
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install binaryen
+cargo install wasm-snip
+pnpm rewriter:build
+pnpm build

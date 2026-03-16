919892b16322e66949863419b21a572c27c60da2
TNXWasTaken <118904290+TNXWasTaken@users.noreply.github.com>
2026-03-16 10:48:05 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 . "$HOME/.cargo/env"
 pnpm i
-cargo install wasm-bindgen
+cargo install wasm-bindgen-cli
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew install binaryen
 cargo install wasm-snip
